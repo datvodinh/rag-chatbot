1636019df5c4553d878d1be44994af39f257fa14
Võ Đình Đạt <vodinhdat03@gmail.com>
2024-03-26 17:00:16 +0700

    feat: add vietnamese prompt

--- src/chatbot/prompt/qa_prompt.py
+++ src/chatbot/prompt/qa_prompt.py
@@ -4,11 +4,17 @@ from llama_index.core.prompts.prompt_type import PromptType
 
 
 def get_qa_and_refine_prompt(language: str) -> tuple[ChatPromptTemplate, ChatPromptTemplate]:
-    return (qa_prompt_en, qa_prompt_refine_en)
+    if language == "eng":
+        return (qa_prompt_en, qa_prompt_refine_en)
+    else:
+        return (qa_prompt_vi, qa_prompt_refine_vi)
 
 
 def get_system_prompt(language: str):
-    return SYSTEM_PROMPT_EN
+    if language == "eng":
+        return SYSTEM_PROMPT_EN
+    else:
+        return SYSTEM_PROMPT_VI
 
 
 SYSTEM_PROMPT_EN = (
@@ -68,6 +74,69 @@ qa_prompt_refine_en = ChatPromptTemplate(
         ),
         ChatMessage(
             content=REFINE_PROMPT_EN,
+            role=MessageRole.USER,
+        ),
+    ],
+    prompt_type=PromptType.REFINE
+)
+
+SYSTEM_PROMPT_VI = (
+    "Bạn là một hệ thống trả lời câu hỏi chuyên nghiệp được tin tưởng trên toàn thế giới.\n"
+    "Luôn trả lời câu hỏi chỉ bằng thông tin ngữ cảnh được cung cấp và không phải kiến thức trước đó.\n"
+    "Một số quy tắc cần tuân thủ:\n"
+    "1. Không bao giờ trực tiếp đề cập đến ngữ cảnh đã cho trong câu trả lời của bạn.\n"
+    "2. Tránh các câu như 'Dựa trên thông tin được cấp, ...' hoặc 'Thông tin ngữ cảnh ...' hoặc bất kỳ điều gì tương tự.\n"
+    "3. Nếu ngữ cảnh không cung cấp thông tin liên quan đến câu hỏi hoặc cung cấp thông tin không đầy đủ, "
+    "hãy trả lời 'Tôi không có đủ thông tin để trả lời câu hỏi.'\n"
+)
+
+USER_PROMPT_VI = (
+    "Thông tin ngữ cảnh được hiển thị dưới đây.\n"
+    "---------------------\n"
+    "{context_str}\n"
+    "---------------------\n"
+    "Dựa trên thông tin trên và không phải kiến thức trước đó, "
+    "hãy trả lời câu hỏi.\n"
+    "Câu hỏi: {query_str}\n"
+    "Trả lời: "
+)
+
+
+REFINE_PROMPT_VI = (
+    "Bạn là một hệ thống trả lời câu hỏi chuyên nghiệp hoạt động một cách nghiêm ngặt trong hai chế độ "
+    "khi reinfing các câu trả lời hiện có:\n"
+    "1. Viết lại một câu trả lời gốc bằng cách sử dụng ngữ cảnh mới.\n"
+    "2. Lặp lại câu trả lời gốc nếu ngữ cảnh mới không hữu ích.\n"
+    "Không bao giờ đề cập đến câu trả lời gốc hoặc ngữ cảnh một cách trực tiếp trong câu trả lời của bạn.\n"
+    "Khi bối rối, chỉ cần lặp lại câu trả lời gốc.\n"
+    "Ngữ cảnh Mới: {context_msg}\n"
+    "Câu hỏi: {query_str}\n"
+    "Câu Trả Lời Gốc: {existing_answer}\n"
+    "Câu Trả Lời Mới: "
+)
+
+qa_prompt_vi = ChatPromptTemplate(
+    [
+        ChatMessage(
+            content=SYSTEM_PROMPT_VI,
+            role=MessageRole.SYSTEM,
+        ),
+        ChatMessage(
+            content=USER_PROMPT_VI,
+            role=MessageRole.USER,
+        ),
+    ],
+    prompt_type=PromptType.QUESTION_ANSWER
+)
+
+qa_prompt_refine_vi = ChatPromptTemplate(
+    [
+        ChatMessage(
+            content=SYSTEM_PROMPT_VI,
+            role=MessageRole.SYSTEM,
+        ),
+        ChatMessage(
+            content=REFINE_PROMPT_VI,
             role=MessageRole.USER,
         ),
     ],
