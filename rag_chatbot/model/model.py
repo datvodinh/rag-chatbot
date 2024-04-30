0ecf5cb682999a253172c17b2278238881508a79
Võ Đình Đạt <vodinhdat03@gmail.com>
2024-04-30 14:54:01 +0700

    perf: update setting

--- rag_chatbot/model/model.py
+++ rag_chatbot/model/model.py
@@ -6,6 +6,9 @@ import requests
 
 load_dotenv()
 
+llm_settings = LLMSettings()
+ollama_settings = OllamaSettings()
+
 
 class LocalRAGModel:
     def __init__(self) -> None:
@@ -13,17 +16,16 @@ class LocalRAGModel:
 
     @staticmethod
     def set(
-        model_name: str = "starling-lm:7b-beta-q5_K_M",
+        model_name: str = "llama3:8b-instruct-q8_0",
         host: str = "host.docker.internal"
     ):
-        llm_settings = LLMSettings()
+
         if model_name in ["gpt-3.5-turbo", "gpt-4"]:
             return OpenAI(
                 model=model_name,
                 temperature=llm_settings.temperature
             )
         else:
-            ollama_settings = OllamaSettings()
             settings_kwargs = {
                 "tfs_z": ollama_settings.tfs_z,
                 "top_k": ollama_settings.top_k,
@@ -33,7 +35,7 @@ class LocalRAGModel:
             }
             return Ollama(
                 model=model_name,
-                base_url=f"http://{host}:11434",
+                base_url=f"http://{host}:{ollama_settings.port}",
                 temperature=llm_settings.temperature,
                 context_window=llm_settings.context_window,
                 request_timeout=ollama_settings.request_timeout,
@@ -45,11 +47,16 @@ class LocalRAGModel:
         payload = {
             "name": model_name
         }
-        return requests.post(f"http://{host}:11434/api/pull", json=payload, stream=True)
+        return requests.post(
+            f"http://{host}:{ollama_settings.port}/api/pull",
+            json=payload, stream=True
+        )
 
     @staticmethod
     def check_model_exist(host: str, model_name: str) -> bool:
-        data = requests.get(f"http://{host}:11434/api/tags").json()
+        data = requests.get(
+            f"http://{host}:{ollama_settings.port}/api/tags"
+        ).json()
         list_model = [d["name"] for d in data["models"]]
         if model_name in list_model:
             return True
--- rag_chatbot/setting/setting.py
+++ rag_chatbot/setting/setting.py
@@ -10,10 +10,10 @@ class OllamaSettings(BaseModel):
     repeat_last_n: int = 64
     repeat_penalty: float = 1.1
     request_timeout: float = 120.0
+    port: int = 11434
 
 
 class LLMSettings(BaseModel):
-    max_new_tokens: int = 256
     context_window: int = 8000
     temperature: float = 0.1
 
@@ -22,7 +22,7 @@ class RetrieverSettings(BaseModel):
     num_queries: int = 5
     similarity_top_k: int = 20
     retriever_weights: List[float] = [0.4, 0.6]
-    top_k_rerank: int = 5
+    top_k_rerank: int = 4
     # rerank_llm: str = "cross-encoder/stsb-roberta-base"
     fusion_mode: str = "dist_based_score"
     chat_token_limit: int = 3000
@@ -30,9 +30,9 @@ class RetrieverSettings(BaseModel):
 
 class IngestionSettings(BaseModel):
     embed_llm: str = "BAAI/bge-large-en-v1.5"
-    embed_batch_size: int = 4
+    embed_batch_size: int = 2
     cache_folder: str = "data/huggingface"
-    chunk_size: int = 512
+    chunk_size: int = 1024
     chunk_overlap: int = 32
     chunking_regex: str = "[^,.;。？！]+[,.;。？！]?"
     paragraph_sep: str = "\n\n\n"
