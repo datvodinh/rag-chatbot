8bb3eceb8d9324ac1892097519fe1e90442c6cdd
Võ Đình Đạt <vodinhdat03@gmail.com>
2024-05-05 21:51:43 +0700

    style: pydantic field in setting

--- rag_chatbot/setting/setting.py
+++ rag_chatbot/setting/setting.py
@@ -1,46 +1,104 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List
 
 
 class OllamaSettings(BaseModel):
-    keep_alive: str = "1h"
-    tfs_z: float = 1.0
-    top_k: int = 40
-    top_p: float = 0.9
-    repeat_last_n: int = 64
-    repeat_penalty: float = 1.1
-    request_timeout: float = 300.0
-    port: int = 11434
+    keep_alive: str = Field(
+        default="1h", description="Keep alive time for the server"
+    )
+    tfs_z: float = Field(
+        default=1.0, description="TFS normalization factor"
+    )
+    top_k: int = Field(
+        default=40, description="Top k sampling"
+    )
+    top_p: float = Field(
+        default=0.9, description="Top p sampling"
+    )
+    repeat_last_n: int = Field(
+        default=64, description="Repeat last n tokens"
+    )
+    repeat_penalty: float = Field(
+        default=1.1, description="Repeat penalty"
+    )
+    request_timeout: float = Field(
+        default=300, description="Request timeout"
+    )
+    port: int = Field(
+        default=11434, description="Port number"
+    )
 
 
 class LLMSettings(BaseModel):
-    context_window: int = 8000
-    temperature: float = 0.1
+    context_window: int = Field(
+        default=8000, description="Context window size"
+    )
+    temperature: float = Field(
+        default=0.1, description="Temperature"
+    )
 
 
 class RetrieverSettings(BaseModel):
-    num_queries: int = 5
-    similarity_top_k: int = 20
-    retriever_weights: List[float] = [0.4, 0.6]
-    top_k_rerank: int = 4
-    rerank_llm: str = "BAAI/bge-reranker-large"
-    fusion_mode: str = "dist_based_score"
-    chat_token_limit: int = 3000
+    num_queries: int = Field(
+        default=5, description="Number of generated queries"
+    )
+    similarity_top_k: int = Field(
+        default=20, description="Top k documents"
+    )
+    retriever_weights: List[float] = Field(
+        default=[0.4, 0.6], description="Weights for retriever"
+    )
+    top_k_rerank: int = Field(
+        default=4, description="Top k rerank"
+    )
+    rerank_llm: str = Field(
+        default="BAAI/bge-reranker-large", description="Rerank LLM model"
+    )
+    fusion_mode: str = Field(
+        default="dist_based_score", description="Fusion mode"
+    )
+    chat_token_limit: int = Field(
+        default=3000, description="Chat memory limit"
+    )
 
 
 class IngestionSettings(BaseModel):
-    embed_llm: str = "BAAI/bge-base-en-v1.5"
-    embed_batch_size: int = 4
-    cache_folder: str = "data/huggingface"
-    chunk_size: int = 1024
-    chunk_overlap: int = 32
-    chunking_regex: str = "[^,.;。？！]+[,.;。？！]?"
-    paragraph_sep: str = "\n\n\n"
-    num_workers: int = 0
+    embed_llm: str = Field(
+        default="BAAI/bge-base-en-v1.5", description="Embedding LLM model"
+    )
+    embed_batch_size: int = Field(
+        default=4, description="Embedding batch size"
+    )
+    cache_folder: str = Field(
+        default="data/huggingface", description="Cache folder"
+    )
+    chunk_size: int = Field(
+        default=1024, description="Document chunk size"
+    )
+    chunk_overlap: int = Field(
+        default=32, description="Document chunk overlap"
+    )
+    chunking_regex: str = Field(
+        default="[^,.;。？！]+[,.;。？！]?", description="Chunking regex"
+    )
+    paragraph_sep: str = Field(
+        default="\n \n", description="Paragraph separator"
+    )
+    num_workers: int = Field(
+        default=0, description="Number of workers"
+    )
 
 
 class StorageSettings(BaseModel):
-    persist_dir_chroma: str = "data/chroma"
-    persist_dir_storage: str = "data/storage"
-    collection_name: str = "data"
-    port: int = 8000
+    persist_dir_chroma: str = Field(
+        default="data/chroma", description="Chroma directory"
+    )
+    persist_dir_storage: str = Field(
+        default="data/storage", description="Storage directory"
+    )
+    collection_name: str = Field(
+        default="collection", description="Collection name"
+    )
+    port: int = Field(
+        default=8000, description="Port number"
+    )
